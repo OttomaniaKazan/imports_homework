71d41ad3ea91143e0e8053abd9fa07e4a7727352
Evgeniy Titov <titevg@gmail.com>
2026-04-30 21:39:06 +0300

    add print current date and time in main module

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from application.salary import print_salary_by_name
 from application.db.people import print_employee_name
+from datetime import datetime
 
 if __name__ == '__main__':
+    now = datetime.now()
+    formatted_date = now.strftime("%A, %d %B %Y, %H:%M")
+    print(f'Текущая дата и время: {formatted_date}\n')
+
     print_employee_name()
     print_salary_by_name()
